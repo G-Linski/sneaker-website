6ba306ad503eabc9e00ad7c9bb01232da39572fd
J <cnbjjj@outlook.com>
2024-03-11 19:59:42 -0500

    add dialog test

--- assets/script/dialog.js
+++ assets/script/dialog.js
@@ -1,5 +1,16 @@
 'use strict';
-
-const dialogContainer = ".dialog";
+const query = (selector) => document.querySelector(selector);
+const event = (on, evt, call) => on.addEventListener(evt, call);
+const dialogContainer = "dialog";
 const dialog = query(dialogContainer);
 
+event(document, 'click', (evt) => {
+    //Temp testing
+    if (evt.pageX < 800)
+        return;
+    if (dialog.open)
+        dialog.close();
+    else
+        dialog.showModal();
+    query('.dialog').classList.toggle("open", dialog.open);
+});
